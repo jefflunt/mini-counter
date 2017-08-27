38c3e39539340e73e0404cd10255a79c13e0b942
jefflunt <jefflunt@gmail.com>
2017-08-27 01:07:31 -0500

    Placeholders for aggregates. Split out functionality of reading, parsing, storing, logging.

--- counter.rb
+++ counter.rb
@@ -1,19 +1,28 @@
-def deep_copy(hash)
-  Marshal.load(Marshal.dump(hash))
-end
+require 'pp'
+
+#def deep_copy(hash)
+#  Marshal.load(Marshal.dump(hash))
+#end
 
 def whole_number?(s)
   s =~ /\A[0-9]+\Z/
 end
 
 def read
-  gets.strip.split(' ', 2)
+  gets
+end
+
+def parse(s)
+  s.strip.split(' ', 2)
+end
+
+def log(s, events)
+  events << [Time.now.utc, s]
 end
 
 def count(num, event, counts)
   if whole_number?(num)
     num = num.to_i
-    counts = deep_copy(counts)
     counts[event] ||= 0
     counts[event] += num
     puts "COUNTED: #{num} #{event}"
@@ -27,8 +36,38 @@ def count(num, event, counts)
   counts
 end
 
+def last_minute_by_second
+end
+
+def last_hour_by_minute
+end
+
+def last_day_by_hour
+end
+
+def last_7_days_by_day
+end
+
+def last_30_days_by_day
+end
+
+def last_year_by_week
+end
+
+def last_year_by_month
+end
+
+def custom_range_by_day(start_date, end_date)
+end
+
 counts = {}
+events = []
 
 loop do
-  counts = count(*read, counts)
+  s = read
+  num, event = parse(s)
+  log(s, events)
+  counts = count(num, event, counts)
+  pp events
+  pp counts
 end
